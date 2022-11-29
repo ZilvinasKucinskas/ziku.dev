89a4efb3e7331c1a4d1549f595cb8f4d7f01b35e
Zilvinas Kucinskas <zil.kucinskas@gmail.com>
2022-11-29 12:29:34 +0200

    Improve navbar and footer

--- src/_components/ui/link.rb
+++ src/_components/ui/link.rb
@@ -1,13 +1,20 @@
 class Ui::Link < Ui::BoxComponent
-  BUTTON_LINK_CLASSES = "inline-flex items-center justify-center p-2 text-sm font-medium rounded-md bg-zinc-100 dark:bg-zinc-800 text-zinc-700 dark:text-zinc-300 hover:bg-zinc-300 hover:text-zinc-800 dark:hover:bg-zinc-700 dark:hover:text-zinc-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-zinc-50 dark:focus:ring-zinc-900".freeze
+  BUTTON_LINK_CLASSES =
+    "inline-flex items-center justify-center p-2
+    text-sm font-medium rounded-md
+    text-zinc-700 dark:text-zinc-300
+    bg-zinc-100 dark:bg-zinc-800
+    hover:bg-zinc-300 hover:text-zinc-800 dark:hover:bg-zinc-700 dark:hover:text-zinc-300
+    focus:outline-none focus:ring-2 focus:ring-inset focus:ring-zinc-50 dark:focus:ring-zinc-900
+    ".freeze
 
   private
 
   def classes
     class_list = []
-    class_list << opts.fetch(:hover, "hover:text-pink-500 dark:hover:text-sky-400")
+    class_list << opts.fetch(:hover, "hover:text-sky-500 dark:hover:text-sky-400")
     class_list << opts.fetch(:weight, "font-semibold")
-    class_list << "!text-pink-500 dark:!text-sky-400" if opts[:active]
+    class_list << "!text-sky-500 dark:!text-sky-400" if opts[:active]
 
     list = button_link? ? BUTTON_LINK_CLASSES : class_list
     list = [] if variant == :none
